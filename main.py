6a1f5333191287402ec1489934d54b54830aca88
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2023-08-04 18:13:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,59 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow.keras as keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import os
+import pickle
 
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Step 1: Load and preprocess the data
-with open('reddit_post_titles.txt', 'r', encoding='utf-8') as file:
-    data = file.readlines()
+# Construct the absolute file path
+file_path = os.path.join(script_dir, 'reddit_post_titles.txt')
 
-data = [title.strip() for title in data if title.strip()]
+# Load the Reddit post titles from the text file
+with open(file_path, 'r', encoding='utf-8') as file:
+    data = file.read().splitlines()
 
-# Step 2: Tokenize the text
+# Preprocess the data
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(data)
-
 total_words = len(tokenizer.word_index) + 1
-input_sequences = tokenizer.texts_to_sequences(data)
 
-# Step 3: Prepare the training data
-input_sequences = np.array(input_sequences)
+# Create input sequences and labels
+input_sequences = []
+for line in data:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+# Pad sequences for equal length
+max_sequence_length = max([len(seq) for seq in input_sequences])
+input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
+
+# Create predictors (input) and labels (output)
 X, y = input_sequences[:, :-1], input_sequences[:, -1]
-sequence_length = X.shape[1]
 y = tf.keras.utils.to_categorical(y, num_classes=total_words)
 
-# Step 4: Build the model
+# Build the model
 model = Sequential()
-model.add(Embedding(total_words, 50, input_length=sequence_length))
-model.add(LSTM(100))
+model.add(Embedding(total_words, 100, input_length=max_sequence_length-1))
+model.add(LSTM(150))
 model.add(Dense(total_words, activation='softmax'))
 
+# Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Step 5: Train the model
-model.fit(X, y, epochs=50, batch_size=128)
+# Train the model
+history = model.fit(X, y, epochs=100, verbose=1)
 
-# Step 6: Generate new titles
-def generate_title(seed_text, next_words=10):
-    for _ in range(next_words):
-        token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen=sequence_length, padding='pre')
-        predicted_index = np.argmax(model.predict(token_list), axis=-1)
-        predicted_word = tokenizer.index_word[predicted_index[0]]
-        seed_text += " " + predicted_word
-    return seed_text
+# Save the trained model, max_sequence_length, and tokenizer
+model.save('reddit_post_titles_model.h5')
+with open('max_sequence_length.txt', 'w') as file:
+    file.write(str(max_sequence_length))
 
-# Example usage:
-generated_title = generate_title("Artificial intelligence")
-print(generated_title)
+with open('tokenizer.pkl', 'wb') as file:
+    pickle.dump(tokenizer, file)
